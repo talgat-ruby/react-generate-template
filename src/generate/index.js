d6870f0b8707b51ea3a76bae2898fd9e473fe95c
Talgat <talgat_ruby@yahoo.com>
2018-01-26 11:20:36 +0600

    Fix merge conflicts in generate

--- src/generate/index.js
+++ src/generate/index.js
@@ -1,14 +1,9 @@
 const path = require('path');
-<<<<<<< HEAD
-=======
-const yargs = require('yargs');
->>>>>>> 813814e296ffb75a69813a3730405bcd12a19bec
 const {messages} = require('../helpers/');
 const {libs, options} = require('./arguments');
 
 const reactFiles = require('./react/');
 
-<<<<<<< HEAD
 (() => {
 	// config yarg
 	const argv = require('yargs')
@@ -74,34 +69,3 @@ const reactFiles = require('./react/');
 	// 		break;
 	// }
 })();
-=======
-const argv = yargs
-	.boolean([options.NO_DIR.name])
-	.alias(options.NO_DIR.name, options.NO_DIR.alias).argv;
-
-// specify path
-let pathArg = argv._[0];
-if (!pathArg) {
-	throw new Error(messages.pathError);
-} else {
-	pathArg = path.resolve(process.cwd(), pathArg);
-}
-
-// specify file name
-const fileName = argv._[1];
-if (!fileName) {
-	throw new Error(messages.fileNameError);
-}
-
-// type of lib
-switch (argv.lib) {
-	case libs.react.name:
-		reactFiles(pathArg, fileName, argv);
-		break;
-	default:
-		throw new Error(
-			messages.libError(Object.values(libs).map(({name}) => name))
-		);
-		break;
-}
->>>>>>> 813814e296ffb75a69813a3730405bcd12a19bec
