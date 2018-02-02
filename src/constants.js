5a011938e1dde219b0ba3ff94db4fd480f4f79af
Talgat <talgat_ruby@yahoo.com>
2018-02-02 19:07:52 +0600

    Add constants

---
+++ src/constants.js
@@ -0,0 +1,7 @@
+const NAME_CONVENTIONS = {
+	KEBAB_CASE: 'kebab-case',
+	UPPER_CAMEL_CASE: 'UpperCamelCase',
+	LOWER_CAMEL_CASE: 'lowerCamelCase',
+	SNAKE_CASE: 'snake_case'
+};
+exports.NAME_CONVENTIONS = NAME_CONVENTIONS;
